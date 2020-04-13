36c91ab0fd501108421a2826ed4f32820fba567a
ocampco <ocampco@gmail.com>
2020-04-14 01:13:02 +1000

    Extract atom packages into own file

---
+++ scripts/atom-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Installing atom packages..."
+apm install autocomplete-modules
+apm install autocomplete-paths
+apm install clipboard-plus
+apm install editorconfig
+apm install file-icons
+apm install highlight-selected
+apm install linter-eslint
+apm install linter-stylelint
+apm install status-bar-blame
+apm install earthsung-syntax-atom
+apm install one-dark-ui
--- scripts/atom.sh
+++ scripts/atom.sh
@@ -3,17 +3,6 @@
 echo "Installing Atom..."
 brew cask atom
 
-echo "Installing atom packages..."
-apm install autocomplete-modules
-apm install autocomplete-paths
-apm install clipboard-plus
-apm install editorconfig
-apm install file-icons
-apm install highlight-selected
-apm install linter-eslint
-apm install linter-stylelint
-apm install status-bar-blame
-apm install earthsung-syntax-atom
-apm install one-dark-ui
+source ./scripts/atom-packages.sh
 
 echo "Please manually install keymap.cson and snippets.cson"
